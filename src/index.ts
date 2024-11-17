6cd09ad9d19e9517d7038e6206e8eff3ea9d209b
tarikyildizci <tarikyyildizci@gmail.com>
2024-11-17 22:11:27 +0300

    release: 0.0.1

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import { createVaretPlugin, createStep, createJob } from '@varet/core'
+
+import { search } from '@inquirer/prompts'
+import fuzzy from '@varet/core/dist/lib/fuzzy'
+
+const OPTIONS = [
+  { value: '🪨 rock' },
+  { value: '📃 paper' },
+  { value: '🔪 scissors' },
+]
+
+const promptStep = createStep({
+  name: 'rock-paper-scissors',
+  handler: async (message) => {
+    const choice = await search({
+      message: 'Rock, paper, scissors? :',
+      source(term) {
+        return term
+          ? fuzzy(OPTIONS, term, (option) => option.value).map(
+              ({ item }) => item,
+            )
+          : OPTIONS
+      },
+    })
+
+    if (choice === OPTIONS[0].value) {
+      console.log('Lol noob.')
+    } else {
+      console.log('i guess im a noob')
+    }
+  },
+  onError: 'skip',
+})
+
+const job = createJob({
+  name: 'rps',
+  description: 'Rock paper scissors',
+  steps: [promptStep],
+})
+
+const plugin = createVaretPlugin({
+  name: '@varet/varet-plugin-rps',
+  description: 'An example Varet CLI Plugin - Rock Paper Scissors',
+  jobs: [job],
+})
+
+export default plugin
